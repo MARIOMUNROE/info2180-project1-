97a51c271ce4776aee80749b93686e3f5846c06d
MARIOMUNROE <mariomunroe7@gmail.com>
2020-10-31 18:16:35 -0500

    Added initial starter files

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const name = document.getElementById('email')
+const form-group = document.getElementById('form')
+
+
+form-group.addEventListener('submit', (e) => {
+  let messages = []
+  if (email.value === '' || email.value == null) {
+    messages.push('Name is required')
+  }
+  
+}
+)
