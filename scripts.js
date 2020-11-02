b361bccf16364d10d20f975a1d2f32cc6150cbc0
MARIOMUNROE <mariomunroe7@gmail.com>
2020-11-01 23:41:07 -0500

    Changes made

--- scripts.js
+++ scripts.js
@@ -1,12 +1,18 @@
-const name = document.getElementById('email')
-const form-group = document.getElementById('form')
-
-
-form-group.addEventListener('submit', (e) => {
-  let messages = []
-  if (email.value === '' || email.value == null) {
-    messages.push('Name is required')
-  }
-  
+window.onload=function(){
+	let btn=document.getElementsByTagName("button")[0];
+	let message=document.getElementsByClassName("message")[0];
+	let email=document.getElementsById("email");
+	
+	btn.addEventListener("click", function(e){
+		e.preventDefault();
+		var email = newemail.value;
+		
+		if (email.indexOf(' ') <=0)
+		{
+			message.textContent="Please enter an email";
+		}
+		else {
+			message.textContent='Thank you! Your email address ${newemail} has been added to our mailing list!';
+		}
+	}
 }
-)
